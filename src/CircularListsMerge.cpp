90b06e0fbb616c57c87df35ec4c69344acb885f5
Varshikakamala <Varshikakamala@missionrnd.com>
2016-04-02 09:24:59 -0700

    Submit Commit

--- src/CircularListsMerge.cpp
+++ src/CircularListsMerge.cpp
@@ -57,18 +57,18 @@ int merge_circularlists(struct node **head1, struct node **head2)
 	if (head1 == NULL && head2 == NULL)  return -1;
 	
 	int i = 0;
-	int l1 = 1, l2 = 1, d1, d2, i = 0, j = 0;
+	int l1 = 0, l2 = 0, d1, d2, i = 0, j = 0;
 	struct node *temp1 = *head1, *temp2 = *head2, *prev = NULL, *n;
 	prev= temp1;
 	n= temp2;
-	temp1 = temp1->next;
+	//temp1 = temp1->next;
 	while (temp1->next != prev)
 	{
 		l1++;
 		temp1 = temp1->next;
 	}
 	temp2 = *head2;
-	temp2 = temp2->next;
+	//temp2 = temp2->next;
 	while (temp2->next != n)
 	{
 		l2++;
